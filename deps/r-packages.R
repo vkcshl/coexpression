d22530633e03779d126acac8df3a784a7fb6306d
Shinjae Yoo <sjyoo@bnl.gov>
2015-12-18 17:34:27 +0000

    Updated bioclite installation path

--- deps/r-packages.R
+++ deps/r-packages.R
@@ -1,3 +1,4 @@
 source("http://bioconductor.org/biocLite.R") 
-biocLite(c("AnnotationDbi", "impute", "GO.db", "preprocessCore")) 
+biocLite(c("RSQLite"), lib = "[% rlib %]" )
+biocLite(c("AnnotationDbi", "impute", "GO.db", "preprocessCore"), lib = "[% rlib %]") 
 install.packages ("WGCNA", repos = "http://cran.wustl.edu", lib = "[% rlib %]")
